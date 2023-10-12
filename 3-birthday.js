c2a4195b60236d3dbbdf1861627d82e42982b3b6
Khausta <anna.a.khaustova@gmail.com>
2023-10-12 12:19:01 +0300

    throw new Error is added to function

--- 3-birthday.js
+++ 3-birthday.js
@@ -1,16 +1,24 @@
 'use strict';
 
  function over14(birthday) {
-    const date = new Date(birthday);
-    if(isNaN(date)) {
-        return 'Невозможно определить возраст';
+    try {
+        const date = new Date(birthday);
+        if(isNaN(date)) {
+            throw new Error('Invalid date format');
+        }
+        date.setFullYear(date.getFullYear() + 14);
+        const now = new Date();
+        return date.getTime() <= now.getTime();
+    } catch(error) {
+        console.log(error + '\n' + 'Дата рождения указана некорректно');
+        return null; //чтобы не выводилось undefined
     }
-    date.setFullYear(date.getFullYear() + 14);
-    const now = new Date();
-    return date.getTime() <= now.getTime();
  }
 
+
+
 //  console.log(over14('2009-10-11')); 
-//  console.log(over14('2009-10-10')); 
-//  console.log(over14('100-2023')); 
+//   console.log(over14('100-2023')); 
 //  console.log(over14('20-10-2009')); 
+//   console.log(over14('2009-10-10')); 
+//   console.log(over14('2009-10-13')); 
