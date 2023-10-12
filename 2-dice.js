768781ca6a3018fc1f189a0f19f8b6d745380ea9
Khausta <anna.a.khaustova@gmail.com>
2023-10-12 11:49:18 +0300

    RegExp is added

--- 2-dice.js
+++ 2-dice.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const dices = ['d4', 'd6', 'd8', 'd10', 'd12', 'd16', 'd20'];
+const dices = [4, 6, 8, 10, 12, 16, 20];
 
 function diceRandomizer(dice) {
-    const diceType = dices.find(dicesEl => dicesEl === dice);
+    const max = Number(dice.replace(/\D/g, ''));
+    const diceType = dices.find(dicesEl => dicesEl === max);
     if (!diceType) {
         return null
     }
-    const max = Number(dice.replace('d', ''));
     return Math.floor(Math.random() * max + 1);
 }
 
