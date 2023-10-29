a403e65a98b09985b7992b649e1ca3b2abd1a99a
Khausta <anna.a.khaustova@gmail.com>
2023-10-29 11:53:23 +0300

    12-promisse-rase

--- 12-promise-race.js
+++ 12-promise-race.js
@@ -2,24 +2,28 @@
 
 async function getProduct(id) {
     const response = await fetch('https://dummyjson.com/products/' + id);
-    return response.json();
+    return response;
 }
 
 async function getProductError(id) {
     const response = await fetch('https://dummyjson.com/productsS/' + id);
-    return response.json();
+    return response;
 }
 
 async function race(arr) {
-    try {
-        return Promise.race(arr);
+    try{
+        const res = await Promise.race(arr);
+        if(!res.ok) {
+            throw new Error(res.status);
+        } 
+        const data  = await res.json();
+        console.log(data);
     } catch (e) {
         console.error(e);
     }
 }
 
-// race([getProductError(2), getProductError(4), getProduct(10)]);
-console.log(race([getProductError(2), getProductError(4), getProduct(10)]))
+race([getProduct(2), getProduct(4), getProduct(10)]);
 
 
 
