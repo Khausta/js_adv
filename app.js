84928b711088b1c7f80ca133d52b4b20cd0fc178
Khausta <anna.a.khaustova@gmail.com>
2023-11-05 23:34:37 +0300

    IIFE modules

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 'use strict';
+/*
+    IIFE 
+    модульный файл js подключается перед главным файлом
+    создается глобальный обхект APP и 
+    в него записываем функции (пример в calc.js);
+*/
+(function (){
+    const a = 1;
+    console.log(APP.calc.add(1, 3));
+    console.log(APP.calc.substruct(1,3));
+})()
 
-console.log('Hello');
+
---
+++ calc.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+const APP = {
+    
+};
+
+(function (){
+    const a = 1;
+
+    function add(f, s) {
+        return f + s;
+    }
+
+    function substruct(f, s) {
+        return f - s;
+    }
+
+    APP.calc = {
+        add,
+        substruct
+    }
+    
+})()
+
+
+
+
