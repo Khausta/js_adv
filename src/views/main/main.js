4ea7691a6eea287bf2c15e86a1706c816858250c
Khausta <anna.a.khaustova@gmail.com>
2023-11-10 17:02:43 +0300

    search element

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,6 +1,7 @@
 import { AbstractView } from "../../common/view.js";
 import onChange from "on-change";
 import { Header } from "../../components/header/header.js";
+import { Search } from "../../components/search/search.js";
 
 export class MainView extends AbstractView {
     state = {
@@ -26,10 +27,11 @@ export class MainView extends AbstractView {
     render() {
         const main = document.createElement('div');
         // main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
+        main.append(new Search(this.state).render());
         this.app.innerHTML = '';
         this.app.append(main);
         this.renderHeader();
-        this.appState.favorites.push('ddd');
+        // this.appState.favorites.push('ddd');
     }
 
     renderHeader() {
